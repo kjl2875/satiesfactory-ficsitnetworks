9075a9891670dc2e95e5587f7b9a881ad180aff5
PUG <kjl2875@gmail.com>
2024-02-25 11:11:31 +0900

    Study 1H
    IF NOT CodeableMerger_C:transferItem(input) THEN keep retry.

--- example-merger.lua
+++ example-merger.lua
@@ -5,6 +5,7 @@
 -- OBJECT: SEND ITEM WHEN RECV THEM.
 
 local merger = component.proxy("C005D0044DF11D169B2717ACEFD70796") -- TODO Change It Your self.
+local counter = 0
 print(merger) -- Object<CodeableMerger_C>
 
 event.ignoreAll()
@@ -12,9 +13,19 @@ event.clear()
 event.listen(merger)
 
 while true do
-    local s_name, o_build, n_dict, s_item = event.pull() -- ItemRequest Object<CodeableMerger_C>
-    if s_name == 'ItemRequest' then
-		local transfered = merger:transferItem(n_dict) -- 0=R,1=M,2=L
-		-- print(transfered) -- true
+	local e, s, p1, p2, p3, p4, p5 = event.pull()
+
+    if merger.hash == s.hash and e == 'ItemRequest' then
+		input = p1 -- 0=R,1=M,2=L
+        item = p2
+
+		while true do
+			local transfered = merger:transferItem(input)
+			if transfered then
+				counter = counter + 1
+				print(counter)
+				break
+			end
+		end
 	end
 end
